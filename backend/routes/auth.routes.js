85b1eab7acc7b560a0fe87137f3510d330a2dd54
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-01-17 03:02:44 +0700

    feat: http docs, validate required fields

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,4 +4,25 @@ app.get("/", (req, res) => {
 	res.send("Auth");
 });
 
+app.post("/signup", (req, res) => {
+	let errors;
+	try {
+		const valid_keys = ["email", "password", "username"];
+
+		// required fields
+		for (const key of valid_keys) {
+			errors = ["key"];
+			if (!req.body[key]) throw new Error(`${key} is required !`);
+		}
+
+		res.json({
+			success: true,
+		});
+	} catch (error) {
+		res.status(500).json({
+			err: errors,
+		});
+	}
+});
+
 module.exports = app;
