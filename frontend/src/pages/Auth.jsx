24af39319ef556705c8d3716dfda1439dd5c90b9
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-01-22 00:47:23 +0700

    fix: form validation

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -6,73 +6,38 @@ import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
-async function signIn() {
-    try {
-        let res = await axios.post(BASE_URL + '/auth/signup', {
-            email: 'rizqy@gmail.com',
-            password: 'test123',
-            username: 'rizqyn9',
-        })
-        return res
-    } catch (error) {
-        console.log(error)
-    }
-}
+// async function signIn() {
+//     try {
+//         let res = await axios.post(BASE_URL + '/auth/signup', {
+//             email: 'rizqy@gmail.com',
+//             password: 'test123',
+//             username: 'rizqyn9',
+//         })
+//         return res
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
 function Auth({ children }) {
     return (
         <div className="bg-violet-800 flex h-screen overflow-hidden justify-center">
-            <div className="py-7 px-8 flex flex-col gap-2 bg-gray-100 w-3/5 min-w-max max-w-sm min-h-[10rem] self-center rounded-xl">
+            <div className="py-7 px-8 flex flex-col gap-2 bg-white w-3/5 min-w-max max-w-sm min-h-[10rem] self-center rounded-xl">
                 {children}
             </div>
         </div>
     )
 }
 
-export function SignIn() {
-    const [loading, setLoading] = useState(false)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setLoading(true)
-        await signIn().then((res) => {
-            alert(JSON.stringify(res))
-        })
-        setLoading(false)
-    }
-
-    return (
-        <Auth>
-            {/* TITLE */}
-            <h1 className="h4 py-3 mb-5 text-center">MASUK</h1>
-            {/* FORM */}
-            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-                <AuthInput name={'test'} type={'email'} label={'Email'} />
-                <AuthInput type={'password'} label={'Password'} />
-                {/* BTN */}
-                <Link to={'/auth/signup'}>Create Account</Link>
-                <div className="flex w-full p-3 items-center justify-center">
-                    <button
-                        type="submit"
-                        className="text-md bg-gray-600 text-white py-2 px-10 hover:bg-gray-800"
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </Auth>
-    )
-}
+const schema = yup
+    .object({
+        name: yup.string().required(),
+        email: yup.string().email('Email not valid').required(),
+        password: yup.string().required('Password is required'),
+    })
+    .required()
 
 export function SignUp() {
-    const schema = yup
-        .object({
-            // name: yup.string().required(),
-            email: yup.string().email().required(),
-            password: yup.string().required(),
-        })
-        .required()
-
     const {
         register,
         handleSubmit,
@@ -91,51 +56,74 @@ export function SignUp() {
             <h1 className="h4 py-3 mb-5 text-center">DAFTAR</h1>
             {/* FORM */}
             <form
-                onSubmit={handleSubmit(onSubmit)}
                 className={'w-full flex flex-col gap-2'}
+                onSubmit={handleSubmit(onSubmit)}
             >
-                <FormInput label={'Name'} name={'name'} placeholder={'Nama'} />
-                <FormInput label={'Email'} name={'email'} />
-                <FormInput label={'Password'} name={'password'} />
+                <FormInput
+                    label={'Name'}
+                    register={register}
+                    name={'name'}
+                    type={'text'}
+                    errors={errors.name?.message}
+                />
+                <FormInput
+                    label={'Email'}
+                    register={register}
+                    name={'email'}
+                    type={'email'}
+                    errors={errors.email?.message}
+                />
+                <FormInput
+                    label={'Password'}
+                    register={register}
+                    name={'password'}
+                    type={'password'}
+                    errors={errors.password?.message}
+                />
+                <Link
+                    to={'/auth/signin'}
+                    className={
+                        'text-sm text-indigo-400 hover:text-indigo-600 italic'
+                    }
+                >
+                    Have account ?
+                </Link>
                 <input
                     type={'submit'}
                     value={'Sign In'}
-                    className={'py-2 mt-5 rounded-lg bg-green-700 text-white'}
+                    className={
+                        'py-2 mt-5 rounded-lg bg-indigo-700 text-white hover:bg-indigo-800 cursor-pointer'
+                    }
                 />
             </form>
         </Auth>
     )
 }
 
-function FormInput({ name, label, type, ...props }) {
+export function SignIn() {
+    return (
+        <Auth>
+            {/* TITLE */}
+            <h1 className="h4 py-3 mb-5 text-center">MASUK</h1>
+        </Auth>
+    )
+}
+
+function FormInput({ name, register, label, errors, type = 'text' }) {
     return (
         <label htmlFor={name} className={'w-full flex flex-col'}>
             <p className="ml-1 pb-1 text-sm font-medium">{label}</p>
             <input
+                name={name}
                 type={type}
-                id={name}
-                {...props}
+                {...register(name)}
                 className={
-                    'py-2 px-3 rounded-md text-white bg-gray-600 outline-2 outline-offset-5 outline-red-200'
+                    'py-2 px-3 rounded-md text-white bg-gray-600 outline-2 outline-offset-5 outline-red-200 '
                 }
             />
+            {errors && (
+                <p className={'text-xs italic text-red-500'}>{errors}</p>
+            )}
         </label>
     )
 }
-const AuthInput = forwardRef(
-    ({ name, type, label, onChange, required = false }, ref) => {
-        return (
-            <label htmlFor={name} className="w-full flex flex-col">
-                <p className="ml-1 pb-1 text-sm font-medium">{label}</p>
-                <input
-                    type={type}
-                    name={name}
-                    id={name}
-                    className="py-2 px-3 rounded-md text-white bg-gray-600"
-                    onChange={onChange}
-                    required={required}
-                />
-            </label>
-        )
-    }
-)
