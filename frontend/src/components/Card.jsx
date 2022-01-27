c0fd9565ced5fe1f07d346e965c5e6406401037d
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-01-27 15:59:32 +0700

    fix: imported static assets

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import a from '/src/static/images/dummy.jpg'
 
 export default function Card() {
     return (
-        <div className="w-[12rem] h-[18rem] p-3 rounded-2xl drop-shadow-md flex flex-col items-center justify-around bg-dark-2">
-            <div className="w-[8rem] h-[8rem] bg-primary rounded-2xl mb-4 overflow-hidden">
-                <img
-                    src="/src/static/images/dummy.jpg"
-                    className="mt-[-4rem]"
-                    alt="dummy"
-                />
+        <div className="w-[10rem] h-[18rem] p-2 py-1 rounded-2xl drop-shadow-md flex flex-col items-center justify-around bg-dark-2">
+            <div className="w-[8rem] h-[8rem] bg-primary rounded-2xl mb-2 overflow-hidden">
+                <img src={a} className="mt-[-4rem]" alt="dummy" />
             </div>
             <div className="flex flex-col gap-2 items-center justify-center">
                 <h2 className="text-md">Americano</h2>
