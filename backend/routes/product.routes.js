484f03f252cd1aa5498bcb3f56f377291463eeab
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-02-24 19:04:30 +0700

    fix: parse json and response /auth/verify

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -3,45 +3,56 @@ const mongoose = require("mongoose");
 const Product = require("../models/Product.model");
 const responses = require("../utils/responses");
 
+/**
+ * Mengambil semua data produk yang ada pada database
+ */
 app.get("/", (req, res) => {
-	try {
-		return Product.find().then((data) => {
-			return responses.success(res, data);
-		});
-	} catch (error) {}
+  try {
+    return Product.find().then((data) => {
+      return responses.success(res, data);
+    });
+  } catch (error) {
+    return responses.fail(res, {}, error);
+  }
 });
 
+/**
+ * Menambah produk ke dalama database
+ */
 app.post("/", (req, res) => {
-	try {
-		const valid_keys = ["menu"];
+  try {
+    const valid_keys = ["menu"];
 
-		for (const key of valid_keys) {
-			if (!req.body[key])
-				return responses.fail(res, (message = `${key} is required`));
-		}
+    for (const key of valid_keys) {
+      if (!req.body[key])
+        return responses.fail(res, (message = `${key} is required`));
+    }
 
-		console.log(req.body.image.file);
-		Product.create({ ...req.body }).then((data, val) => {
-			console.log(val);
-		});
+    console.log(req.body.image.file);
+    Product.create({ ...req.body }).then((data, val) => {
+      console.log(val);
+    });
 
-		return responses.success(res, "created success");
-	} catch (error) {}
+    return responses.success(res, "created success");
+  } catch (error) {}
 });
 
+/**
+ * Mengupdate data produk, dari parameter yang berikan
+ */
 app.post("/:id", (req, res) => {
-	const { id } = req.params;
-	try {
-		// Is valid Object ID
-		if (!mongoose.isValidObjectId(id))
-			return responses.fail(res, "Object ID not valid");
+  const { id } = req.params;
+  try {
+    // Is valid Object ID
+    if (!mongoose.isValidObjectId(id))
+      return responses.fail(res, "Object ID not valid");
 
-		Product.findByIdAndUpdate(String(id)).then((data) => {
-			return responses.success(res, data);
-		});
-	} catch (error) {
-		return responses.error(res, "Server error");
-	}
+    Product.findByIdAndUpdate(String(id)).then((data) => {
+      return responses.success(res, data);
+    });
+  } catch (error) {
+    return responses.error(res, "Server error");
+  }
 });
 
 module.exports = app;
