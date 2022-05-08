04a8220d07d6788038f5dad88b4eccea71c1b3a1
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-05-08 07:44:23 +0700

    feat: promos controller

--- backend/models/Promo.model.js
+++ backend/models/Promo.model.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose"
 
 const PromosSchema = new mongoose.Schema({
-  products: Schema.Types.Mixed,
-  price: Schema.Types.Number,
-  orderTotal: Schema.Types.Number,
+  productsList: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Products",
+    },
+  ],
+  discount: mongoose.Schema.Types.Number,
+  orderTotal: mongoose.Schema.Types.Number,
 })
 
 export default mongoose.model("Promos", PromosSchema)
--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -7,6 +7,7 @@ import TransactionRouter from "./transaction.routes.js"
 import ProductRouter from "./product.routes.js"
 import AdminRouter from "./admin.routes.js"
 import CustomerRouter from "./customer.routes.js"
+import PromoRouter from "./promo.routes.js"
 
 import { VerifyToken } from "../middleware/token.js"
 
@@ -20,5 +21,6 @@ app.use("/transaction", TransactionRouter)
 app.use("/products", ProductRouter)
 app.use("/admin", AdminRouter)
 app.use("/customer", CustomerRouter)
+app.use("/promo", PromoRouter)
 
 export default app
