6172487d2daffd018167cf77c84b82c8c1cf5876
rizqyn9 <rizqynugroho9@gmail.com>
2022-07-29 23:15:47 +0700

    feat: anything

--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -1,17 +1,25 @@
 import { Router } from "express"
-import { parseOrder } from "@/lib/parseOrder"
-import * as TransactionController from "@/controller/transaction.controller"
+import * as transactionController from "@/controller/transaction.controller"
 import * as productController from "@/controller/product.controller"
 import { isValidKeyRequest } from "@/utils"
+import z from "zod"
+import {
+  CreateSchema,
+  OrderSchema,
+  orderSchema,
+  transactionSchema,
+} from "@/utils/schema"
+import { createSchema } from "../utils/schema"
+import mongoose from "mongoose"
 
 const app = Router()
 
 /* -------------------------- Get all transactions -------------------------- */
 app.get("/", async (req, res, next) => {
   try {
-    await TransactionController.getAll().then((payload) =>
-      res.json({ payload })
-    )
+    await transactionController
+      .getAll()
+      .then((payload) => res.json({ payload }))
   } catch (error) {
     next(error)
   }
@@ -20,43 +28,45 @@ app.get("/", async (req, res, next) => {
 /* ------------------------- Create new transaction ------------------------- */
 app.post("/create", async (req, res, next) => {
   try {
-    isValidKeyRequest(["orders", "transaction"], req)
-    const { orders, transaction } = req.body
+    const validator = createSchema.safeParse(req.body)
 
-    const parsedOrder = parseOrder(orders)
+    if (!validator.success) throw new Error("Request not falid")
 
-    // Update total order menu
-    const listOrderId = await Promise.all(
-      Object.entries(parsedOrder).map(([key, val]) =>
-        productController.incrementOrderById(key, val.quantity)
-      )
+    const { orders, transaction } = validator.data
+
+    const mapQuantity = new Map<string, OrderSchema>()
+
+    Object.entries(orders).forEach(([_, val]) => {
+      if (mapQuantity.has(val._id)) {
+        const current = mapQuantity.get(val._id)!
+        mapQuantity.set(val._id, {
+          ...current,
+          quantity: current.quantity + val.quantity,
+        })
+      } else mapQuantity.set(val._id, val)
+    })
+
+    /* --------------------- Update quantity ordered product -------------------- */
+    const updateProduct = await Promise.all(
+      Array.from(mapQuantity.values()).map((val) => {
+        return productController.incrementOrderById(val._id, val.quantity)
+      })
     )
-    // .then(async (val) => {
-    //   return await TransactionModel.create({
-    //     orderList: val.map((data) => data._id),
-    //     price: transaction.price,
-    //     paymentMethod: transaction.paymentMethod,
-    //     promo: isValidObjectId(transaction.promo, false) || null,
-    //     discount: transaction.discount,
-    //   })
-    //     .then((data) => data)
-    //     .catch((err) => {
-    //       console.log(err)
-    //       throw new Error("Transaction cant created")
-    //     })
-    // })
-    // .catch((err) => {
-    //   console.log(err)
-    //   throw new Error("Update product error")
-    // })
 
-    console.log(listOrderId)
+    /* --------------------------- Create transaction --------------------------- */
+    const transactionModel = await transactionController.create({
+      orderList: updateProduct.map((val) => val._id),
+      price: transaction.total,
+      paymentMethod: transaction.method,
+    })
 
     res.json({
       msg: "Success create transaction",
-      payload: listOrderId,
+      payload: transactionModel,
     })
   } catch (error) {
+    console.log({ error })
+
     next(error)
   }
 })
