a16135327bb83abf3c0f46e3113274c079817edd
rizqyn9 <rizqynugroho9@gmail.com>
2022-08-21 17:52:43 +0700

    feat: generate apriori table

--- frontend/src/pages/Apriori.tsx
+++ frontend/src/pages/Apriori.tsx
@@ -1,29 +1,56 @@
 import { Button } from '../components/Button'
 import { GridRow } from '../components/Grid'
 import { TD, TH, TR } from '../components/Table'
-import { H1 } from '../components/Typography'
 import { useState } from 'react'
 import { useAxios } from '../hooks/useAxios'
+import { axiosPrivate } from '../services'
+import React from 'react'
+
+type ResponseApriori = {
+    payload: {
+        itemsets:
+            | {
+                  support: number
+                  items: string[]
+              }[]
+            | null
+    }
+}
 
 export default function Apriori() {
-    const [apriori, setApriori] = useState([])
-    const fetcher = useAxios()
-    const test = () => {
-        fetcher.get('/another-test')
+    const [promoState, setPromoState] = React.useState<NewPromo>({ isActive: false, price: 0, menuId: null })
+    const [apriori, setApriori] = useState<NonNullable<ResponseApriori['payload']['itemsets']>>([])
+    const handleGenerate = () => {
+        axiosPrivate.get<ResponseApriori>('/apriori').then((val) => {
+            let data = val.data.payload
+            if (data && typeof data == 'object' && data.itemsets) {
+                console.log(val)
+                setApriori(data.itemsets)
+            }
+        })
     }
+
+    const handleOpenNewPromo = () => setPromoState({ ...promoState, isActive: true })
+
     return (
         <GridRow className="px-5 w-full flex-auto overflow-x-scroll text-sm" title="Analitycs">
             <div className="py-8 overflow-x-scroll">
-                <Button onClick={test}>asdads</Button>
-                <H1>Get Promo Recomendation</H1>
+                <div className="mb-5">
+                    {promoState.isActive ? (
+                        <AddNewPromoCard promoState={promoState} setPromoState={setPromoState} />
+                    ) : (
+                        <Button onClick={handleOpenNewPromo}>Add new promo</Button>
+                    )}
+                </div>
+                <h3 className="text-lg font-bold mb-3">Get Promo Recomendation</h3>
                 <div className="bg-pimary flex flex-col gap-5">
-                    <p>Result</p>
                     <div className="overflow-hidden rounded-md border-2 border-white">
                         <table className="w-full bg-dark-2 rounded-lg">
                             <thead>
                                 <tr className="bg-primary">
+                                    <TH>No</TH>
                                     <TH>Menu</TH>
-                                    <TH>Order History</TH>
+                                    <TH>Support</TH>
                                 </tr>
                             </thead>
                             <tbody className="border-2 border-white">
@@ -36,11 +63,21 @@ export default function Apriori() {
                                 ) : (
                                     apriori.map((val, i) => (
                                         <TR key={i}>
-                                            <TD className="p-2 text-center" width={100}>
+                                            <TD className="p-2 text-center" width={50}>
                                                 {i + 1}
                                             </TD>
-                                            <TD className="flex gap-5">
-                                                <Button className="w-1/3">Remove</Button>
+                                            <TD className="p-2 text-center" width={400}>
+                                                <div className="flex gap-4">
+                                                    {val.items.map((val) => (
+                                                        <span key={val}>{val}</span>
+                                                    ))}
+                                                </div>
+                                            </TD>
+                                            <TD className="">
+                                                {val.support}
+                                                {/* <Button className="" size="sm">
+                                                    Remove
+                                                </Button> */}
                                             </TD>
                                         </TR>
                                     ))
@@ -48,10 +85,37 @@ export default function Apriori() {
                             </tbody>
                         </table>
                     </div>
-                    <Button>Generate</Button>
+                    <Button onClick={handleGenerate}>Generate</Button>
                 </div>
                 <div className="flex flex-col gap-8 text-white mb-6"></div>
             </div>
         </GridRow>
+    )
+}
+
+type NewPromo = {
+    isActive: boolean
+    price: number
+    menuId: string | null
+}
+type NewPromoCardProps = {
+    promoState: NewPromo
+    setPromoState: (data: NewPromo) => void
+}
+function AddNewPromoCard(props: NewPromoCardProps) {
+    const { promoState, setPromoState } = props
+    return (
+        <div className="flex gap-4 flex-col mb-5 p-5">
+            Add New Promo
+            <input className="bg-dark-2 p-3 rounded-xl" placeholder="Harga" />
+            <div className="flex gap-4">
+                <Button size="sm" onClick={() => setPromoState({ ...promoState, isActive: false })}>
+                    Cancel
+                </Button>
+                <Button size="sm" onClick={() => setPromoState({ ...promoState, isActive: false })}>
+                    Tambah
+                </Button>
+            </div>
+        </div>
     )
 }
